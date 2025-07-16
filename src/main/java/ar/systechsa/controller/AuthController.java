b79ea1c82200134147e104d19c28c72f978de534
Gonzalo Sola <gsola@systechsa.com>
2025-07-16 18:04:46 -0300

    Elimino Response y agrego un nuevo controller de Auth. Tengo que continuar con esta como prioridad

--- src/main/java/ar/systechsa/controller/AuthController.java
+++ src/main/java/ar/systechsa/controller/AuthController.java
@@ -1,43 +1,43 @@
 package ar.systechsa.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-@Tag(name = "Check", description = "Check API")  
-@RestController (value = "checkController")
-@RequestMapping(value = "/ping")
+@Tag(name = "AuthController",
+    description = "Autorizacion para identificar a la persona")  
+@RestController (value = "authController")
+@RequestMapping(value = "/auth")
 @SecurityScheme(name = "bearerAuth",
     type = SecuritySchemeType.HTTP,
     bearerFormat = "JWT",
     description = "Bearer Authentication for API access",
     scheme = "bearer")
-public class PingController {
+public class AuthController {
 
-    public static final Logger logger = LoggerFactory.getLogger(PingController.class);
+    public static final Logger logger = LoggerFactory.getLogger(AuthController.class);
 
-    @GetMapping(path = "")
-    @Operation(description = "Endpoint para chequear que esta levantado", 
+    @PostMapping
+    @Operation(description = "Chequear que esta levantado",
         responses = {
             @ApiResponse(responseCode = "200", description = "Respuesta exitosa"),
             @ApiResponse(responseCode = "500", description = "Error interno del servidor")
         }
     )
     @SecurityRequirement(name = "bearerAuth")
-    public ResponseEntity<String> get() {
-        logger.info("PingController.get - Endpoint called");
-        return ResponseEntity.status(HttpStatus.OK).body("pong");
+    public ResponseEntity<?> postAuth() {
+        logger.info("AuthController.postAuth - Endpoint called");
+        return ResponseEntity.status(HttpStatus.OK).body(true);
     }
-        
+
 }
